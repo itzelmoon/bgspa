354787a29770451d917382da33ca4991b7b14f2b
Alam Atzin <alamatzin@ciencias.unam.mx>
2022-09-13 19:14:33 -0500

    chore | Valide los campos de mensaje y confirmar correo, idente y organice un poco el script.

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,18 +1,41 @@
 let btnSubmit = document.getElementById("btnValidar");
-let campoTelefono = document.getElementById("validationNumber");
 
 btnSubmit.addEventListener("click", function(e){ 
     e.preventDefault();
+    let campoMensaje = document.getElementById("campoMensaje");
+    let campoTelefono = document.getElementById("validationNumber");
+    let confirmarCorreo = document.getElementById("validationCorreo");
 
-    
-    
-//Validacion Telefono
-if((campoTelefono.value.length == 10) && (!isNaN(campoTelefono.value))) {
-    campoTelefono.classList.remove("is-invalid");
-    campoTelefono.classList.add("is-valid");
-} else {
-    campoTelefono.classList.remove("is-valid");
-    campoTelefono.classList.add("is-invalid");
-}
+    //Validacion Telefono
+    if((campoTelefono.value.length == 10) && (!isNaN(campoTelefono.value))) {
+        campoTelefono.classList.remove("is-invalid");
+        campoTelefono.classList.add("is-valid");
+    } else {
+        campoTelefono.classList.remove("is-valid");
+        campoTelefono.classList.add("is-invalid");
+    }
 
+    // Validacion Mensaje
+    if (campoMensaje.value.length >= 10) { //validacion Nombre
+        campoMensaje.classList.remove("is-invalid");
+        campoMensaje.classList.add("is-valid");
+    } else {
+        campoMensaje.classList.remove("is-valid");
+        campoMensaje.classList.add("is-invalid");
+    }
+
+
+
+
+
+
+
+      //confirmacion correo
+      if (valorcorreo === confirmar){
+        confirmarCorreo.classList.remove("is-invalid");
+        confirmarCorreo.classList.add("is-valid");
+    } else {
+        confirmarCorreo.classList.remove("is-valid");
+        confirmarCorreo.classList.add("is-invalid");
+    }
 });
