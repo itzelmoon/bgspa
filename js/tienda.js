402bbe9b1c1d72ee381a9503c677dee398fefc92
Eduardo Barajas <ing.barajas.perez@gmail.com>
2022-09-23 00:06:06 -0500

    chore | Se agregó elementos al html y productos al js.

--- js/tienda.js
+++ js/tienda.js
@@ -41,3 +41,38 @@ addItem({'name':'Removedor de cuticula',
     'precio':'250',
     'categoria':'Básicos',
     'id':'5'});
+
+    addItem({'name':'Lija de Pie',
+    'img':'/img/Productos/Basicos/LIJAPIE.png',
+    'description':'',
+    'precio':'',
+    'categoria':'Básicos',
+    'id':'6'});
+
+addItem({'name':'BIO JELLY - Tropical',
+    'img':'/img/Productos/Bio Jelly Spa/BIO_JELLY_90g(2).png',
+    'description':'',
+    'precio':'',
+    'categoria':'Bio Jelly Spa',
+    'id':'7'});
+
+addItem({'name':'BIO JELLY - Menta',
+    'img':'/img/Productos/Bio Jelly Spa/BIO_JELLY_90g(3).png',
+    'description':'',
+    'precio':'',
+    'categoria':'Bio Jelly Spa',
+    'id':'8'});
+
+addItem({'name':'BIO JELLY - Coco',
+    'img':'/img/Productos/Bio Jelly Spa/BIO_JELLY_90g(4).png',
+    'description':'',
+    'precio':'',
+    'categoria':'Bio Jelly Spa',
+    'id':'9'});
+
+addItem({'name':'BIO JELLY - Té verde',
+    'img':'/img/Productos/Bio Jelly Spa/BIO_JELLY_90g(5).png',
+    'description':'90 g',
+    'precio':'', 
+    'categoria':'Bio Jelly Spa',
+    'id':'10'});
