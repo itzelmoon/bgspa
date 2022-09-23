9d91029fe7da30155089e0c92708d89251ba0cfa
Alam Atzin <alamatzin@ciencias.unam.mx>
2022-09-22 23:15:38 -0500

    chore | Se agrego navegacion y footer, tambien se inicio los checkbox de categorias y se agrego algunos productos en js

---
+++ js/tienda.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+addItem({'name':'Aceite para masaje',
+    'img':'/img/Productos/Aceite para Masaje/ACEITE_MASAJE_ALMENDRAS.png',
+    'description':'Almendras',
+    'precio':'200', 
+    'categoria':'Aceites para masaje',
+    'id':'1'});
+
+addItem({'name':'Aceite para masaje',
+    'img':'/img/Productos/Aceite para Masaje/ACEITE_MASAJE_TORONJA.png',
+    'description':'Toronja',
+    'precio':'200',
+    'categoria':'Aceites para masaje',
+    'id':'2'});
+
+addItem({'name':'Ablandador de callos',
+    'img':'/img/Productos/Basicos/ABLANDADOR_CALLOS.png',
+    'description':'15 ml',
+    'precio':'200',
+    'categoria':'Básicos',
+    'id':'3'});
+
+addItem({'name':'Aceite para cuticula',
+    'img':'/img/Productos/Basicos/ACEITE_CUTICULA.png',
+    'description':'15 ml',
+    'precio':'250',
+    'categoria':'Básicos',
+    'id':'4'});
+
+addItem({'name':'Removedor de cuticula',
+    'img':'/img/Productos/Basicos/BG_SPA_BASICOS_REMOVEDOR_DE_CUTICULA.png',
+    'description':'15 ml',
+    'precio':'250',
+    'categoria':'Básicos',
+    'id':'5'});
