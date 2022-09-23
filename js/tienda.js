2938be1a11af3326cabca43ffa6e8f80deab715f
alexanderDelgado <aledt2103@gmail.com>
2022-09-23 01:27:09 -0500

    chores | se añadió modal para imagen y descripción, directorio productos, hoja de estilo

--- js/tienda.js
+++ js/tienda.js
@@ -59,7 +59,51 @@ function addItem(item){
     </div>
   </div>
 
+  <!-- Modal -->
+  <div class="modal fade" id="modal_${item.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">${item.name}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <img src="${item.img}" class="card-img-top" alt="..." />
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
+
+  <!-- Modal -->
+<div class="modal fade" id="modal1_${item.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-md modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${item.name}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ${item.description}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+
+    const itemsContainer = document.getElementsByClassName("list-items")[0];
+    itemsContainer.innerHTML += itemHTML;
+}
 
 
 
