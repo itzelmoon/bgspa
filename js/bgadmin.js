b4e29f14fd64d2b7bae518b209ead47ba6fdaa6a
Nayeli Trujilo <nay.tm28@gmail.com>
2022-09-25 14:47:01 -0500

    Chore | Se agregó arreglo productos y estructura de tabla.

--- js/bgadmin.js
+++ js/bgadmin.js
@@ -13,7 +13,28 @@ btnSubmit.addEventListener("click", function(e){
     let precio = document.getElementById("campoPrecio");
     let descripcion = document.getElementById("campoDescripcion");
 
+    let arregloProductos = {
+        "id" : cont,
+        "name": nombre.value,
+        "img": archivo.value,
+        "descripcion": descripcion.value,
+        "precio": parseFloat(precio.value),
+        "categoria": categoria.value
+    };
 
+    cont++;
+    ident++;
+    productos.push(arregloProductos);
+    console.log(productos);
+
+
+    let id_row = 'row' + cont; //
+    let fila_todascolumnas = `<tr id= ${id_row} row-sm-12 row-md-12 ><td> ${ident} </td><td>        
+    ${nombre.value} </td><td> ${archivo.value} </td><td> ${descripcion.value} </td><td>$ ${parseFloat(precio.value)} </td><td> ${categoria.value} </td></tr>`;
+   
+    //Agregar a la tabla
+    $("#lista").append(fila_todascolumnas); //
+      
 
 
 });
