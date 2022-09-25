0155c4866f9db95ccac5508abc188db0712e6d13
Valeria Leza Sanchez <valerialeza.s@gmail.com>
2022-09-25 14:41:07 -0500

    chore: | Se definierón variables y se agregó botón btnSubmi

---
+++ js/bgadmin.js
@@ -0,0 +1,19 @@
+let btnSubmit = document.getElementById("btnAgregar");
+let productos = [];
+let cont = 1;
+let ident = 0;
+let lista = document.getElementById("lista");
+const key = "info";
+
+btnSubmit.addEventListener("click", function(e){ 
+    e.preventDefault();
+    let nombre = document.getElementById("campoProducto");
+    let archivo = document.getElementById("campoArchivo");
+    let categoria = document.getElementById("inlineFormCustomSelect");
+    let precio = document.getElementById("campoPrecio");
+    let descripcion = document.getElementById("campoDescripcion");
+
+
+
+
+});
